6db0db55b3de254e86c56fe26dc6ed09526db0da
Crakchi <crakchi@gmail.com>
2025-04-08 09:56:04 -0500

    feat(UPS-01): Se agergaron las clases heredadas de Persona (Administrativo, Estudiante, Docente, Visitante)

--- src/main/java/ec/edu/ups/clases/Administrativo.java
+++ src/main/java/ec/edu/ups/clases/Administrativo.java
@@ -1,17 +1,44 @@
 package ec.edu.ups.clases;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Administrativo extends Persona{
     private List<String> cargos;
     private List<String> responsabilidades;
 
-    public Administrativo() {}
+    public Administrativo() {
+        cargos = new ArrayList();
+        responsabilidades = new ArrayList();
+    }
 
-    public Administrativo(String cedula, String nombre, String apellido, String telefono, String correoElectronico, List<String> cargos, List<String> responsabilidades) {
+    public Administrativo(String cedula, String nombre, String apellido, String telefono, String correoElectronico) {
         super(cedula, nombre, apellido, telefono, correoElectronico);
+        cargos = new ArrayList();
+        responsabilidades = new ArrayList();
+    }
+
+    public List<String> getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(List<String> cargos) {
         this.cargos = cargos;
+    }
+
+    public List<String> getResponsabilidades() {
+        return responsabilidades;
+    }
+
+    public void setResponsabilidades(List<String> responsabilidades) {
         this.responsabilidades = responsabilidades;
     }
 
+    @Override
+    public String toString() {
+        return "Administrativo{" +
+                "cargos=" + cargos +
+                ", responsabilidades=" + responsabilidades +
+                '}';
+    }
 }
---
+++ src/main/java/ec/edu/ups/clases/Docente.java
@@ -0,0 +1,44 @@
+package ec.edu.ups.clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Docente extends Persona {
+    private List<String> titulosAcademicos;
+    private List<String> areasDeEspeciializacion;
+
+    public Docente() {
+        titulosAcademicos = new ArrayList<String>();
+        areasDeEspeciializacion = new ArrayList<>();
+    }
+
+    public Docente(String cedula, String nombre, String apellido, String telefono, String correoElectronico) {
+        super(cedula, nombre, apellido, telefono, correoElectronico);
+        titulosAcademicos = new ArrayList<String>();
+        areasDeEspeciializacion = new ArrayList<>();
+    }
+
+    public List<String> getTitulosAcademicos() {
+        return titulosAcademicos;
+    }
+
+    public void setTitulosAcademicos(List<String> titulosAcademicos) {
+        this.titulosAcademicos = titulosAcademicos;
+    }
+
+    public List<String> getAreasDeEspeciializacion() {
+        return areasDeEspeciializacion;
+    }
+
+    public void setAreasDeEspeciializacion(List<String> areasDeEspeciializacion) {
+        this.areasDeEspeciializacion = areasDeEspeciializacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Docente{" +
+                "titulosAcademicos=" + titulosAcademicos +
+                ", areasDeEspeciializacion=" + areasDeEspeciializacion +
+                '}';
+    }
+}
